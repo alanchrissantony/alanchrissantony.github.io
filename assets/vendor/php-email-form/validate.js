332e97cca2784ccb98738c1d5a7ad35c366d8b0f
Alan Chriss Antony <83913615+alanchrissantony@users.noreply.github.com>
2022-01-19 19:55:50 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -18,7 +18,9 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
+        //displayError(thisForm, 'The form action property is not set!')
+        thisForm.querySelector('.sent-message').classList.add('d-block');
+        thisForm.reset();
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
