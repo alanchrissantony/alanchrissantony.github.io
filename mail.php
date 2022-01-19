5fd650a46c2767d2033d93caa5e28b3a8599fbd8
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-01-19 15:05:37 +0530

    first commit

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "youremail@mail.com";
+$subject = $_POST['subject'];
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@alanchrissantony.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
